7d9352652d7d1e9abd8cb586423ab02e2a2bb1cc
Dan Tran <96944395+dan-tran-van@users.noreply.github.com>
2026-04-18 08:08:24 +0000

    feat: update CORS settings to allow additional origins and make callback URL configurable

--- server/server/auth/api/google_login_view.py
+++ server/server/auth/api/google_login_view.py
@@ -1,3 +1,5 @@
+import os
+
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
@@ -5,5 +7,7 @@ from dj_rest_auth.registration.views import SocialLoginView
 
 class GoogleLogin(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
-    callback_url = "http://localhost:3000/auth/google/callback"
+    callback_url = (
+        os.getenv("GOOGLE_CALLBACK_URL") or "http://localhost:3000/auth/google/callback"
+    )
     client_class = OAuth2Client
